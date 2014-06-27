f51cba705499ba39221c0d7932acc78d9173a192
Eran Hammer <eran@hueniverse.com>
2014-06-26 23:07:27 -0700

    empty template

---
+++ static/questions.js
@@ -0,0 +1,36 @@
+var questions = [
+    {
+        title: 'Name a kind of club that people join?',
+        answers: [
+            ['Gym/Health', 42],
+            ['Book', 17],
+            ['Social/Elks', 10],
+            ['Country/Gold', 8],
+            ['Social service/YMCA', 5],
+            ['Youth/Scouts', 3],
+            ['Shopping', 2]
+        ]
+    },
+    {
+        title: 'Which US state doesn\'t get much tourism?',
+        answers: [
+            ['Idaho', 14],
+            ['Utah', 9],
+            ['Alaska', 6],
+            ['Iowa', 6],
+            ['Louisiana', 6]
+        ]
+    },
+    {
+        title: 'Name your best asset as an employee?',
+        answers: [
+            ['Hard worker', 23],
+            ['Dependable', 13],
+            ['Punctual/Promot', 10],
+            ['Good personality', 10],
+            ['Honest/Loyal', 7],
+            ['Creative', 4],
+            ['Patient', 3]
+        ]
+    }
+];
